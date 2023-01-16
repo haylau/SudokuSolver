a13682ab8aab8209ddce51a71f59708c81f80192
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-01-16 15:39:37 -0800

    inital commit

---
+++ inc/sudoku.h
@@ -0,0 +1,12 @@
+/**
+ * @file sudoku.h
+ * @author Hayden Lauritzen (haydenlauritzen@gmail.com)
+ * @date 2022-12-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
---
+++ src/sudoku.c
@@ -0,0 +1,14 @@
+/**
+ * @file sudoku.c
+ * @author Hayden Lauritzen (haydenlauritzen@gmail.com)
+ * @date 2022-12-20
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "../inc/sudoku.h"
+
+int main(char* argc, char* argv[]) {
+    
+}
