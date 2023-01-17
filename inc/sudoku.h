542f01f3c91de138d0059ae7854a058443604580
Hayden Lauritzen <haydenlauritzen@gmail.com>
2023-01-17 03:15:43 -0800

    solves easy puzzles

--- inc/sudoku.h
+++ inc/sudoku.h
@@ -7,47 +7,86 @@
  *
  */
 
-#include <math.h> // sqrt
+#include <math.h>    // sqrt
 #include <stdbool.h> // bool
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h> // threads
 
  /**
-  *  Board Structure
-  * grid[row][col]
-  *   1 2 3 4 5 6 7 8 9 col
-  * 1 - - - - - - - - -
-  * 2 - - - - - - - - -
-  * 3 - - - - - - - - -
-  * 4 - - - - - - - - -
-  * 5 - - - - - - - - -
-  * 6 - - - - - - - - -
-  * 7 - - - - - - - - -
-  * 8 - - - - - - - - -
-  * 9 - - - - - - - - -
-  * row
-  *
-  * 1 2 3
-  * 4 5 6
-  * 7 8 9
-  * grid
-  *
-  */
+ *  Board Structure
+ * grid[row][col]
+ *   1 2 3 4 5 6 7 8 9 col
+ * 1 - - - - - - - - -
+ * 2 - - - - - - - - -
+ * 3 - - - - - - - - -
+ * 4 - - - - - - - - -
+ * 5 - - - - - - - - -
+ * 6 - - - - - - - - -
+ * 7 - - - - - - - - -
+ * 8 - - - - - - - - -
+ * 9 - - - - - - - - -
+ * row
+ *
+ *   1 2 3 4
+ * 1 - - - -
+ * 2 - - - -
+ * 3 - - - -
+ * 4 - - - -
+ *
+ * 1 2
+ * 3 4
+ *
+ * 1 2 3
+ * 4 5 6
+ * 7 8 9
+ *
+ *
+ *
+ * 1 - sqrt(psize)
+ * - - -
+ * - - psize
+ * grid
+ *
+*/
 
-  // checks for all subsets of the puzzle
+// threaded functions for validation
+void* solveRow(void* args);
+void* solveCol(void* args);
+void* solveGrid(void* args);
+// threaded functions for solving
 void* checkRow(void* args);
 void* checkCol(void* args);
 void* checkGrid(void* args);
 
+// missing number information 
+typedef struct missing_t {
+  int rows;
+  int cols;
+  int grids;
+} missing_t;
+
 // storage for pthreads
-typedef struct {
+typedef struct checkpuzzle_t {
   int idx;
   int psize;
   int** grid;
   bool valid;
   bool complete;
-} t_checkpuzzle;
+  int numMissing;
+} checkpuzzle_t;
+
+// helper functions
+inline int getGridIdx(int row, int col, int psize);
+inline bool isSolvable(missing_t* missingNums, int psize);
+
+typedef struct solvepuzzle_t {
+  int idx;
+  int psize;
+  int** grid;
+  missing_t* missingNums;
+} solvepuzzle_t;
+
 
 // takes puzzle size and grid[][] representing sudoku puzzle
 // and tow booleans to be assigned: complete and valid.
@@ -56,7 +95,10 @@ typedef struct {
 // A puzzle is complete if it can be completed with no 0s in it
 // If complete, a puzzle is valid if all rows/columns/boxes have numbers from 1
 // to psize For incomplete puzzles, we cannot say anything about validity
-void checkPuzzle(int psize, int** grid, bool* complete, bool* valid);
+// returns int[3][10] of num missing in each subsection
+missing_t* checkPuzzle(int psize, int** grid, bool* complete, bool* valid);
+
+void solvePuzzle(missing_t* missingNums, int psize, int** grid);
 
 // takes filename and pointer to grid[][]
 // returns size of Sudoku puzzle and fills grid
--- src/sudoku.c
+++ src/sudoku.c
@@ -9,75 +9,240 @@
 
 #include "../inc/sudoku.h"
 
-void* checkRow(void* arg) {
-    t_checkpuzzle* status = (t_checkpuzzle*)arg;
-    bool numCount[10] = { 0 }; // keep track of used nums
-    int numMissing = 0;
-    for (int col = 1; col <= status->psize; ++col) {
-        int num = status->grid[status->idx][col];
-        if (num == 0) {
-            // puzzle is not complete, but might be valid
-            ++numMissing;
-            status->complete = false;
+int getGridIdx(int row, int col, int psize) {
+    int gridSize = (int)floor(sqrt(psize));
+    return (((row - 1) / gridSize) * gridSize) + ((col - 1) / gridSize) + 1;
+}
+
+bool isSolvable(missing_t* missingNums, int psize) {
+    for (int i = 0; i < psize; ++i) {
+        if (missingNums[i].rows == 1 || missingNums[i].cols == 1 || missingNums[i].grids == 1) {
+            return true;
         }
-        else if (numCount[num]) {
-            // doubled number puzzle is not valid nor complete
-            status->valid = false;
-            status->complete = false;
-            return NULL;
+    }
+    return false;
+}
+
+void* solveRow(void* args) {
+    solvepuzzle_t* params = (solvepuzzle_t*)args;
+    bool* numCount = (bool*)calloc(params->psize, sizeof(bool)); // keep track of used nums
+    int numIdx, missingNum;
+    int row = params->idx;
+    for (int col = 1; col <= params->psize; ++col) {
+        int num = params->grid[row][col];
+        if (num == 0) {
+            // location of missing number
+            numIdx = col;
         }
         else {
             // first occurance of number
             numCount[num] = true;
         }
     }
+    for (int i = 1; i <= params->psize; ++i) {
+        if (!numCount[i]) missingNum = i;
+    }
+    params->grid[row][numIdx] = missingNum;
+    // update missing_t
+    --(params->missingNums[row - 1].rows);
+    --(params->missingNums[numIdx - 1].cols);
+    --(params->missingNums[getGridIdx(row, numIdx, params->psize) - 1].grids);
     return NULL;
-};
-void* checkCol(void* arg) {
-    t_checkpuzzle* status = (t_checkpuzzle*)arg;
-    bool numCount[10] = { 0 }; // keep track of used nums
-    int numMissing = 0;
-    for (int row = 1; row <= status->psize; ++row) {
-        int num = status->grid[row][status->idx];
+}
+
+void* solveCol(void* args) {
+    solvepuzzle_t* params = (solvepuzzle_t*)args;
+    bool* numCount = (bool*)calloc(params->psize, sizeof(bool)); // keep track of used nums
+    int numIdx, missingNum;
+    int col = params->idx;
+    for (int row = 1; row <= params->psize; ++row) {
+        int num = params->grid[row][col];
         if (num == 0) {
-            // puzzle is not complete, but might be valid
-            ++numMissing;
-            status->complete = false;
-        }
-        else if (numCount[num]) {
-            // doubled number puzzle is not valid nor complete
-            status->valid = false;
-            status->complete = false;
-            return NULL;
+            // location of missing number
+            numIdx = row;
         }
         else {
             // first occurance of number
             numCount[num] = true;
         }
     }
+    for (int i = 1; i <= params->psize; ++i) {
+        if (!numCount[i]) missingNum = i;
+    }
+    params->grid[numIdx][col] = missingNum;
+    // update missing_t
+    --(params->missingNums[numIdx - 1].rows);
+    --(params->missingNums[col - 1].cols);
+    --(params->missingNums[getGridIdx(numIdx, col, params->psize) - 1].grids);
     return NULL;
-};
-void* checkGrid(void* arg) {
-    t_checkpuzzle* status = (t_checkpuzzle*)arg;
-    bool numCount[10] = { 0 }; // keep track of used nums
-    int numMissing = 0;
-    int gridSize = (int)floor(sqrt(status->psize));
+}
+void* solveGrid(void* args) {
+    solvepuzzle_t* params = (solvepuzzle_t*)args;
+    bool* numCount = (bool*)calloc(params->psize, sizeof(bool)); // keep track of used nums
+    int rowIdx, colIdx, missingNum;
+    int grid = params->idx;
+    int gridSize = (int)floor(sqrt(params->psize));
     for (int i = 0; i < gridSize; ++i) {
         // converts grid idx to grid inital row idx
-        int row = ((status->idx - 1) / 3 * 3 + 1) + i;
+        int row = (((params->idx - 1) / gridSize) * gridSize + 1) + i;
         for (int j = 0; j < gridSize; ++j) {
             // converts grid idx to grid inital col idx
-            int col = ((status->idx % 3 == 1) ? 1 : ((status->idx % 3 == 2) ? 4 : 7)) + j;
-            int num = status->grid[row][col];
+            int col = (((params->idx - 1) % gridSize) * gridSize + 1) + j;
+            int num = params->grid[row][col];
             if (num == 0) {
                 // puzzle is not complete, but might be valid
-                ++numMissing;
-                status->complete = false;
+                rowIdx = row;
+                colIdx = col;
+            }
+            else {
+                // first occurance of number
+                numCount[num] = true;
+            }
+        }
+    }
+    for (int i = 1; i <= params->psize; ++i) {
+        if (!numCount[i]) missingNum = i;
+    }
+    params->grid[rowIdx][colIdx] = missingNum;
+    // update missing_t
+    --(params->missingNums[rowIdx - 1].rows);
+    --(params->missingNums[colIdx - 1].cols);
+    --(params->missingNums[grid - 1].grids);
+    return NULL;
+}
+
+void solvePuzzle(missing_t* missingNums, int psize, int** grid) {
+    while (isSolvable(missingNums, psize)) {
+        bool* working = (bool*)calloc(psize, sizeof(bool));
+        pthread_t solveThreads[psize];
+        solvepuzzle_t args[psize];
+        // spawn row threads
+        for (int i = 0; i < psize; ++i) {
+            if (missingNums[i].rows == 1) { // there is a easy solve here
+                args[i].idx = i + 1;
+                args[i].psize = psize;
+                args[i].grid = grid;
+                args[i].missingNums = missingNums;
+                working[i] = true;
+                pthread_create(&solveThreads[i], NULL, solveRow, &args[i]);
+            }
+        }
+        // join row threads
+        for (int i = 0; i < psize; ++i) {
+            if (working[i]) {
+                pthread_join(solveThreads[i], NULL);
+                working[i] = false;
+            }
+
+        }
+        // spawn col threads
+        for (int i = 0; i < psize; ++i) {
+            if (missingNums[i].cols == 1) { // there is a easy solve here
+                args[i].idx = i + 1;
+                args[i].psize = psize;
+                args[i].grid = grid;
+                args[i].missingNums = missingNums;
+                working[i] = true;
+                pthread_create(&solveThreads[i], NULL, solveCol, &args[i]);
+            }
+        }
+        // join col threads
+        for (int i = 0; i < psize; ++i) {
+            if (working[i]) {
+                pthread_join(solveThreads[i], NULL);
+                working[i] = false;
+            }
+        }
+        // spawn grid threads
+        for (int i = 0; i < psize; ++i) {
+            if (missingNums[i].grids == 1) { // there is a easy solve here
+                args[i].idx = i + 1;
+                args[i].psize = psize;
+                args[i].grid = grid;
+                args[i].missingNums = missingNums;
+                working[i] = true;
+                pthread_create(&solveThreads[i], NULL, solveGrid, &args[i]);
+            }
+        }
+        // join grid threads
+        for (int i = 0; i < psize; ++i) {
+            if (working[i]) {
+                pthread_join(solveThreads[i], NULL);
+                working[i] = false;
+            }
+        }
+        free(working);
+    }
+}
+
+void* checkRow(void* args) {
+    checkpuzzle_t* params = (checkpuzzle_t*)args;
+    // keep track of used nums
+    bool* numCount = (bool*)calloc(params->psize, sizeof(bool));
+    for (int col = 1; col <= params->psize; ++col) {
+        int num = params->grid[params->idx][col];
+        if (num == 0) {
+            // puzzle is not complete, but might be valid
+            ++(params->numMissing);
+            params->complete = false;
+        }
+        else if (numCount[num]) {
+            // doubled number puzzle is not valid nor complete
+            params->valid = false;
+            params->complete = false;
+            return NULL;
+        }
+        else {
+            // first occurance of number
+            numCount[num] = true;
+        }
+    }
+    free(numCount);
+    return NULL;
+};
+void* checkCol(void* args) {
+    checkpuzzle_t* params = (checkpuzzle_t*)args;
+    bool* numCount = (bool*)calloc(params->psize, sizeof(bool)); // keep track of used nums
+    for (int row = 1; row <= params->psize; ++row) {
+        int num = params->grid[row][params->idx];
+        if (num == 0) {
+            // puzzle is not complete, but might be valid
+            ++(params->numMissing);
+            params->complete = false;
+        }
+        else if (numCount[num]) {
+            // doubled number puzzle is not valid nor complete
+            params->valid = false;
+            params->complete = false;
+            return NULL;
+        }
+        else {
+            // first occurance of number
+            numCount[num] = true;
+        }
+    }
+    return NULL;
+};
+void* checkGrid(void* args) {
+    checkpuzzle_t* params = (checkpuzzle_t*)args;
+    bool* numCount = (bool*)calloc(params->psize, sizeof(bool)); // keep track of used nums
+    int gridSize = (int)floor(sqrt(params->psize));
+    for (int i = 0; i < gridSize; ++i) {
+        // converts grid idx to grid inital row idx
+        int row = (((params->idx - 1) / gridSize) * gridSize + 1) + i;
+        for (int j = 0; j < gridSize; ++j) {
+            // converts grid idx to grid inital col idx
+            int col = (((params->idx - 1) % gridSize) * gridSize + 1) + j;
+            int num = params->grid[row][col];
+            if (num == 0) {
+                // puzzle is not complete, but might be valid
+                ++(params->numMissing);
+                params->complete = false;
             }
             else if (numCount[num]) {
                 // doubled number puzzle is not valid nor complete
-                status->valid = false;
-                status->complete = false;
+                params->valid = false;
+                params->complete = false;
                 return NULL;
             }
             else {
@@ -89,17 +254,18 @@ void* checkGrid(void* arg) {
     return NULL;
 };
 
-void checkPuzzle(int psize, int** grid, bool* complete, bool* valid) {
+missing_t* checkPuzzle(int psize, int** grid, bool* complete, bool* valid) {
 
+    missing_t* missing = (missing_t*)malloc(sizeof(missing_t) * psize);
     // arguments for thread function
-    t_checkpuzzle rows[9];
-    t_checkpuzzle cols[9];
-    t_checkpuzzle grids[9];
+    checkpuzzle_t rows[psize];
+    checkpuzzle_t cols[psize];
+    checkpuzzle_t grids[psize];
 
     // open threads for every subset of the puzzle
-    pthread_t rowThreads[9];
-    pthread_t colThreads[9];
-    pthread_t gridThreads[9];
+    pthread_t rowThreads[psize];
+    pthread_t colThreads[psize];
+    pthread_t gridThreads[psize];
     for (int i = 0; i < psize; ++i) {
         // open row thread
         rows[i].idx = i + 1;
@@ -107,6 +273,7 @@ void checkPuzzle(int psize, int** grid, bool* complete, bool* valid) {
         rows[i].grid = grid;
         rows[i].valid = true;
         rows[i].complete = true;
+        rows[i].numMissing = 0;
         pthread_create(&rowThreads[i], NULL, checkRow, &rows[i]);
         // open column thread
         cols[i].idx = i + 1;
@@ -114,14 +281,16 @@ void checkPuzzle(int psize, int** grid, bool* complete, bool* valid) {
         cols[i].grid = grid;
         cols[i].valid = true;
         cols[i].complete = true;
-        pthread_create(&colThreads[i], NULL, checkRow, &cols[i]);
+        cols[i].numMissing = 0;
+        pthread_create(&colThreads[i], NULL, checkCol, &cols[i]);
         // open grid thread
         grids[i].idx = i + 1;
         grids[i].psize = psize;
         grids[i].grid = grid;
         grids[i].valid = true;
         grids[i].complete = true;
-        pthread_create(&gridThreads[i], NULL, checkRow, &grids[i]);
+        grids[i].numMissing = 0;
+        pthread_create(&gridThreads[i], NULL, checkGrid, &grids[i]);
     }
 
     // join all threads
@@ -135,9 +304,14 @@ void checkPuzzle(int psize, int** grid, bool* complete, bool* valid) {
     *valid = true;    // if all rows/columns/boxes have numbers from 1 to psize
     *complete = true; // if it can be completed with no 0s in it
     for (int i = 0; i < psize; ++i) {
-        if (!rows[i].valid && !cols[i].valid && !grids[i].valid) *valid = false;
-        if (!rows[i].complete && !cols[i].complete && !grids[i].complete) *complete = false;
+        if (!rows[i].valid || !cols[i].valid || !grids[i].valid) *valid = false;
+        if (!rows[i].complete || !cols[i].complete || !grids[i].complete) *complete = false;
+        missing[i].rows = rows[i].numMissing;
+        missing[i].cols = cols[i].numMissing;
+        missing[i].grids = grids[i].numMissing;
     }
+
+    return missing;
 }
 
 int readSudokuPuzzle(char* filename, int*** grid) {
@@ -190,14 +364,19 @@ int main(int argc, char** argv) {
     int sudokuSize = readSudokuPuzzle(argv[1], &grid);
     bool valid = false;
     bool complete = false;
-    checkPuzzle(sudokuSize, grid, &complete, &valid);
+    missing_t* missing = checkPuzzle(sudokuSize, grid, &complete, &valid);
     printf("Complete puzzle? ");
     printf(complete ? "true\n" : "false\n");
     if (complete) {
         printf("Valid puzzle? ");
         printf(valid ? "true\n" : "false\n");
     }
+    else if (valid) {
+        printf("Attempting Solve.\n");
+        solvePuzzle(missing, sudokuSize, grid);
+    }
     printSudokuPuzzle(sudokuSize, grid);
+    free(missing);
     deleteSudokuPuzzle(sudokuSize, grid);
     return EXIT_SUCCESS;
 }
